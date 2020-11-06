568a2cdf2ec3538da4d36be460beff1d5a740c98
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-11-06 16:27:19 +0100

    init project

---
+++ readme.md
@@ -0,0 +1,5 @@
+![GitHub repo size](https://img.shields.io/github/repo-size/ganatrade/api)
+![GitHub issues](https://img.shields.io/github/issues-raw/ganatrade/api)
+![GitHub pull requests](https://img.shields.io/github/issues-pr-raw/ganatrade/api)
+# API - Ganatrade
+
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const api = express();
+const router = express.Router();
+
+api.use(router);
+api.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+api.use(bodyParser.json());
+api.use(cors());
+
+api.get("/", function (req, res) {
+    res.send("Hello world, welcome to V1 api.");
+})
+
+api.listen(8888, () => {
+    console.log(`App listening at http://localhost:8888`)
+})
