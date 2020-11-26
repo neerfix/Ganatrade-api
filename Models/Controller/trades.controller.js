d15acdd6f971446d33c9f9f5675a6f7a8c0e39f1
Antoine Mousset <amousset@secuserve.com>
2020-11-26 14:32:54 +0100

    Auto stash before rebase of "origin/feat/routes"

---
+++ Models/Controller/trades.controller.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const userService = require("../Services/user.service");
+
+// routes
+router.post("/authenticate", authenticate);
+router.get("/", getAll);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService
+        .authenticate(req.body)
+        .then((user) =>
+            user
+                ? res.json(user)
+                : res.status(404).json({ message: "Trade not found" })
+        )
+        .catch((err) => next(err));
+}
+
+function getAll(req, res, next) {
+    userService
+        .getAll()
+        .then((users) => res.json(users))
+        .catch((err) => next(err));
+}
