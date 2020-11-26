f79b0a8bdab89656e841a7071ab6e9bd68121e79
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-11-26 11:45:06 +0100

    feat: api with jwt and route to User

--- server.js
+++ server.js
@@ -1,68 +1,26 @@
-const admin = require('firebase-admin');
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const api = express();
-const router = express.Router();
-const pjson = require('./package.json');
-let serviceAccount;
-require('custom-env').env(process.env.NODE_ENV)
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('utils/jwt');
+const errorHandler = require('utils/error-handler');
 
-if(process.env.NODE_ENV !== 'production'){
-    serviceAccount = require('./config/serviceAccount-beta.json');
-}else{
-    serviceAccount = require('./config/serviceAccount.json');
-}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-api.use(router);
-api.use(bodyParser.urlencoded({
-    extended: false
-}));
+// use JWT auth to secure the api
+app.use(jwt());
 
-api.use(bodyParser.json());
-api.use(cors());
+// api routes
+app.use('/users', require('./Models/Controller/users.controller'));
 
-const errorMessage = (reason, message, type, detail) => {
-    return {
-        "reason": reason,
-        "message": message,
-        "type": type,
-        "detail": detail
-    }
-};
+// global error handler
+app.use(errorHandler);
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.DATABASE_URL
+// start server
+const port = process.env.NODE_ENV === 'production' ? 8888 : 8080;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
-
-const db = admin.firestore();
-
-api.get("/", function (req, res) {
-    (async () => {
-        try {
-            const document = db.collection('info').doc('phFgIOlGtN9HEzl7PfH9');
-            let data = await document.get();
-            let response = data.data();
-            if(response === undefined){
-                return res.status(404).send(errorMessage('Resource was not found', 'The data at the specified id does not exist.'));
-            }else{
-                return res.status(200).send(
-                    {
-                        "project": pjson.name,
-                        "author": pjson.author,
-                        "version": pjson.version,
-                        "description": pjson.description,
-                        response
-                    }
-                );
-            }
-        } catch (error) {
-            return res.status(500).send(errorMessage(error.code, error.message));
-        }
-    })();
-})
-
-api.listen(process.env.PORT, () => {
-    console.log(`App listening at http://localhost:`+process.env.PORT)
-})
