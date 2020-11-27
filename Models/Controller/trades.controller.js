e4ea8cfb971356581575d1f9e94b39424269192d
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2020-11-27 10:53:10 +0100

    Feat/routes (#6)
    * Added docs/Global.md
    
    * Modified 2 files
    
    * Modified 2 files
    
    * Modified 3 files
    
    * Modified reference/API.v1.yaml
    
    * Reviews
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * Modified reference/API.v1.yaml
    
    * feat: api with jwt and route to User
    
    * fix: firebase export and add routes to Users
    
    * fix: add serviceAccount + script to lunch api
    
    * fix: script to make start
    
    * fix: install nodemon
    
    * feat: add routes to categories
    
    * fix: update function in user services
    
    * Auto stash before rebase of "origin/feat/routes"
    
    * Auto stash before rebase of "origin/feat/routes"
    
    WIP
    
    * fix: return fake category and data
    
    * Add trades api route
    
    * feat: add route to following:
    
    * fix: make file open localhost serve
    
    * Add reviews api route
    
    * some fixes
    
    Co-authored-by: Nicolas Notararigo <nicolas.notararigo@gmail.com>
    Co-authored-by: antoinemousset1999@gmail.com <antoinemousset1999@gmail.com>
    Co-authored-by: Antoine Mousset <amousset@secuserve.com>

---
+++ Models/Controller/trades.controller.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const tradeService = require("../Services/trades.services");
+
+// routes
+router.get("/", getAllTrades);
+router.post("/", createNewTrade);
+router.get("/:tradeId", getOneTradeById);
+router.patch("/:tradeId", updateTradeById);
+router.delete("/:tradeId", deleteTradeById);
+
+module.exports = router;
+
+function getAllTrades(req, res, next) {
+    tradeService
+        .getAllTrades()
+        .then((trades) => res.json(trades))
+        .catch((err) => next(err));
+}
+
+function createNewTrade(req, res, next) {
+    tradeService
+        .createNewTrade()
+        .then(() => res.json("Ok"))
+        .catch((err) => next(err));
+}
+
+function updateTradeById(req, res, next) {
+    tradeService
+        .updateTradeById()
+        .then(() => res.json("Ok"))
+        .catch((err) => next(err));
+}
+
+function deleteTradeById(req, res, next) {
+    tradeService
+        .deleteTradeById()
+        .then(() => res.json("Ok"))
+        .catch((err) => next(err));
+}
+
+function getOneTradeById(req, res, next) {
+    tradeService
+        .getOneTradeById()
+        .then(() => res.json("Ok"))
+        .catch((err) => next(err));
+}
---
+++ Models/Services/reviews.services.js
@@ -0,0 +1,36 @@
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+// users hardcoded for simplicity, store in a db for production applications
+const reviews = [{ id: 1, username: 'pingouin', password: 'singe', firstName: 'oiseau', lastName: 'cachalot' }];
+
+module.exports = {
+    getAllReviews,
+    getOneReviewById,
+    createNewReview,
+    updateReviewById,
+    deleteReviewById
+};
+
+async function getAllReviews() {
+    return reviews.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
+
+async function createNewReview() {
+    return "201";
+}
+
+async function updateReviewById() {
+    return "201";
+}
+
+async function deleteReviewById() {
+    return "201";
+}
+
+async function getOneReviewById() {
+    return "201";
+}
