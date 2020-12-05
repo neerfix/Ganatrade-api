3294df6099a833da916bfc2f69cd5b0bb441ff69
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-12-05 16:54:59 +0100

    feat: add route to create a new Offer

--- Models/Services/offers.services.js
+++ Models/Services/offers.services.js
@@ -1,5 +1,3 @@
-const config = require('config.json');
-const jwt = require('jsonwebtoken');
 const db = require('../../utils/firebase');
 
 module.exports = {
@@ -29,15 +27,38 @@ async function getAllOffers() {
     }
 }
 
-async function createNewOffer(req) {
-    const document = db.collection('offers').doc(req.params.offerId);
-    let response = (await document.get()).data();
-
-    if(!response){
-        return {code: 404, message: "Offer not found"}
-    }
-
-    return response;
+async function createNewOffer(req, res) {
+    await db.collection('offers').add({
+        user_id: req.body.user_id,
+        title: req.body.title,
+        product: {
+          name: req.body.product.name,
+          condition: req.body.product.condition
+        },
+        description: req.body.description ? req.body.description : "",
+        pictures: req.body.pictures ? req.body.pictures : "",
+        category: req.body.category,
+        tags: req.body.tags,
+        trade: {
+          method: req.body.trade.method,
+          target: req.body.trade.target ? req.body.trade.target : "",
+          estimation: req.body.trade.estimation ? req.body.trade.estimation : "",
+          place: req.body.trade.place ? req.body.trade.place : "",
+          radius: req.body.trade.radius ? req.body.trade.radius : "",
+        },
+        views: 0,
+        saves: 0,
+        is_active: true,
+        created_at: new Date(Date.now()),
+        updated_at: new Date(Date.now())
+    }).then(result =>{
+        db.collection('offers').doc(result.id).update({
+            id: result.id
+        });
+        return res.status(202).send(' Successfully created a new offer : ' + result.id + " => " + req.body.title);
+    }).catch(e => {
+        return res.status(409).send({ e });
+    });
 }
 
 async function updateOfferById(req) {
