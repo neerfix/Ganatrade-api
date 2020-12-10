fb311d61d9d8fbc0219bf01d5269755e4339ab1d
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2020-12-10 19:31:50 +0100

    Feat/authenticate (#13)
    * Add users routes
    
    * fix: double db
    
    * feat: routes to create a new user
    
    Co-authored-by: Antoine Mousset <amousset@secuserve.com>

--- Models/Services/users.services.js
+++ Models/Services/users.services.js
@@ -1,9 +1,9 @@
 const config = require('config.json');
 const jwt = require('jsonwebtoken');
 const db = require('../../utils/firebase');
-
-// users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const admin = require('firebase-admin');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
     authenticate,
@@ -14,6 +14,7 @@ module.exports = {
     deleteUserById
 };
 
+//TODO
 async function authenticate({ username, password }) {
     const user = users.find(u => u.username === username && u.password === password);
     if (user) {
@@ -31,9 +32,9 @@ async function getAllUsers() {
         const data = db.collection('users');
         let response = [];
         await data.get().then(querySnapshot => {
-            let users = querySnapshot.docs;
-            for (let user of users) {
-                response.push(user.data());
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+                response.push(doc.data());
             }
         });
         return response;
@@ -56,15 +57,41 @@ async function getOneUserById(req) {
     return response;
 }
 
-async function createNewUser() {
-    const document = db.collection('users').doc(req.params.userId);
-    let response = (await document.get()).data();
-
-    if(!response){
-        return {code: 404, message: "User not found"}
-    }
-
-    return response;
+async function createNewUser(req, res) {
+    admin.auth().createUser({
+        email: req.body.email,
+        emailVerified: false,
+        phoneNumber: req.body.phoneNumber,
+        password: req.body.password,
+        displayName: req.body.firstName + " " + req.body.lastName,
+        photoURL: req.body.avatar,
+        disabled: false,
+    })
+        .then(function(userRecord) {
+            db.collection('users').doc(userRecord.uid)
+                .set({
+                    id: userRecord.uid,
+                    delete_profile: false,
+                    created_at: new Date(userRecord.metadata.creationTime),
+                    last_login: new Date(userRecord.metadata.creationTime),
+                    email: req.body.email,
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    username: req.body.username,
+                    phone: req.body.phoneNumber ? req.body.phoneNumber : "",
+                    avatar: req.body.avatar,
+                    rank: "trader",
+                    private_profile: false,
+                }).then(result =>{
+                return result;
+            }).catch(e => {
+                return  e;
+            });
+        })
+        .catch(function(error) {
+        console.log('Error creating new user : ', error.message);
+      return error
+    })
 }
 
 async function updateUserById() {
