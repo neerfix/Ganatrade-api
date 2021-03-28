6fd774d279a24d0adfaa32190a1cbca39100dcc3
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2021-03-28 15:53:03 +0200

    fix: mierda

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,7 @@
 const admin = require('firebase-admin');
 let serviceAccount;
 
-if(process.env.NODE_ENV !== 'production'){
-    serviceAccount = require('../config/serviceAccount.json');
-}
+serviceAccount = require('../../config/serviceAccount.json');
 
 admin.initializeApp(serviceAccount);
 
